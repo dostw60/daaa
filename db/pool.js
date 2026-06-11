b9cf2d1e38e927b84b6175fd80adea87643c1f5d
dostw60 <dostmagab@gmail.com>
2026-06-11 16:13:08 +0545

    Add files via upload

--- db/pool.js
+++ db/pool.js
@@ -1,25 +1,8 @@
 const { Pool } = require("pg");
 
-const connectionConfig = process.env.DATABASE_URL
-  ? {
-      connectionString: process.env.DATABASE_URL,
-      ssl:
-        process.env.NODE_ENV === "production"
-          ? { rejectUnauthorized: false }
-          : false,
-    }
-  : {
-      user: process.env.DB_USER,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
-      ssl:
-        process.env.NODE_ENV === "production"
-          ? { rejectUnauthorized: false }
-          : false,
-    };
-
-const pool = new Pool(connectionConfig);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 module.exports = pool;
